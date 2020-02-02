33fac489407e1ef53c132a6f97d8651f909ca278
Mavuks <vuks.m2rten1@gmail.com>
2020-02-02 13:41:09 +0200

    minimum changes

--- app.js
+++ app.js
@@ -13,18 +13,14 @@ function solve() {
     var kindlustustootaja = 0.016;
     var tulumaks = 0.1928;
 
-
     if (math_opt[0].checked == true) {
 
             bruto = (summa *100) / ((1 + brutosots  + brutokindlustustooandja)*100);
             neto = bruto - ((bruto * pension) + (bruto * kindlustustootaja) + (bruto * tulumaks));
 
-
-
-        document.getElementById('neto').innerHTML = neto.toFixed(2);
-        document.getElementById('bruto').innerHTML = bruto.toFixed(2);
-        document.getElementById('tooandja').innerHTML = summa;
-
+            document.getElementById('neto').innerHTML = neto.toFixed(2);
+            document.getElementById('bruto').innerHTML = bruto.toFixed(2);
+            document.getElementById('tooandja').innerHTML = summa;
 
 
     } else if (math_opt[1].checked == true) {
@@ -32,24 +28,18 @@ function solve() {
             tooandja = summa  * (1 + brutosots + brutokindlustustooandja);
             neto = summa - ((summa*pension)  + (summa * kindlustustootaja)  + (summa * tulumaks));
 
-
             document.getElementById('neto').innerHTML = neto.toFixed(2);
             document.getElementById('bruto').innerHTML = summa;
             document.getElementById('tooandja').innerHTML = tooandja.toFixed(2);
 
-
-
     } else if (math_opt[2].checked == true) {
 
             bruto = (summa  * 100) / ((1 - tulumaks - pension - kindlustustootaja)*100);
-
             tooandja = bruto  * (1 + brutosots + brutokindlustustooandja);
-
 
             document.getElementById('neto').innerHTML = summa;
             document.getElementById('bruto').innerHTML = bruto.toFixed(2);
             document.getElementById('tooandja').innerHTML = tooandja.toFixed(2);
-
 
     }
     return false;
